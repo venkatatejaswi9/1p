2f87535f93820bb97288d1bf344349dd7c0e9664
venkatatejaswi9 <41776370+venkatatejaswi9@users.noreply.github.com>
2018-08-29 12:16:45 +0530

    Create lrn.py

---
+++ lrn.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if (a>=b and a>=c):
+    num1=a
+    print(num1)
+elif(b>=a and b>=c):
+    num2=b
+    print(num2)
+else:
+    num3=c
+    print(num3)
+        
+    
+        
